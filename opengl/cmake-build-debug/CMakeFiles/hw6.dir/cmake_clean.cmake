b85bcb5ef047c4eca2292ad517d04ea73cedc000
JolinQChen <qiaolingchen1@gmail.com>
2019-11-18 11:04:22 -0500

    chess
    pieces are represented by simple entities, press'0' to enable light0, press '1' to enable light1, press 'd' to move eye location down, press'u' to move eye location up

---
+++ opengl/cmake-build-debug/CMakeFiles/hw6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hw6.dir/main.cpp.o"
+  "hw6"
+  "hw6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hw6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ opengl/main.cpp
@@ -0,0 +1,574 @@
+#include <iostream>
+
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h> // standard definitions
+#include <OpenGL/gl.h>
+#include <GLUT/glut.h>
+
+
+#define ESC 27
+
+//----------------------------------------------------------------------
+// Global variables
+//
+// The coordinate system is set up so that the (x,y)-coordinate plane
+// is the ground, and the z-axis is directed upwards. The y-axis points
+// to the north and the x-axis points to the east.
+//
+// The values (x,y) are the current camera position. The values (lx, ly)
+// point in the direction the camera is looking. The variables angle and
+// deltaAngle control the camera's angle. The variable deltaMove
+// indicates the amount of incremental motion for the camera with each
+// redraw cycle. The variables isDragging and xDragStart are used to
+// monitor the mouse when it drags (with the left button down).
+//----------------------------------------------------------------------
+
+
+
+
+// Camera position
+float x = 0.0, y = 0.0; // initially 5 units south of origin
+float deltaMove = 0.0; // initially camera doesn't move
+float x_board = 0.0, y_board = 0.0;
+
+// Camera direction
+float lx = 0.0, ly = 0.0, lz = 0.0; // camera points initially along y-axis
+float angle = 0.0; // angle of rotation for the camera direction
+float deltaAngle = 0.0; // additional angle change when dragging
+bool light0 = false;
+bool light1 = false;
+// Mouse drag control
+int isDragging = 0; // true when dragging
+int xDragStart = 0; // records the x-coordinate when dragging starts
+
+// map
+
+
+//----------------------------------------------------------------------
+// Reshape callback
+//
+// Window size has been set/changed to w by h pixels. Set the camera
+// perspective to 45 degree vertical field of view, a window aspect
+// ratio of w/h, a near clipping plane at depth 1, and a far clipping
+// plane at depth 100. The viewport is the entire window.
+//
+//----------------------------------------------------------------------
+void init(void)
+{
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_shininess[] = { 50.0 };
+    GLfloat light_position[] = { 1.0, 1.0, 1.0, 0.0 };
+    glClearColor(0.0, 0.0, 0.0, 0.0);
+    glShadeModel(GL_SMOOTH);
+
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_DEPTH_TEST);
+}
+
+
+void changeSize(int w, int h)
+{
+    float ratio = ((float)w) / ((float)h); // window aspect ratio
+    glMatrixMode(GL_PROJECTION); // projection matrix is active
+    glLoadIdentity(); // reset the projection
+    gluPerspective(45.0, ratio, 0.1, 100.0); // perspective transformation
+    glMatrixMode(GL_MODELVIEW); // return to modelview mode
+    glViewport(0, 0, w, h); // set viewport (drawing area) to entire window
+}
+
+//----------------------------------------------------------------------
+// Draw one snowmen (at the origin)
+//
+// A snowman consists of a large body sphere and a smaller head sphere.
+// The head sphere has two black eyes and an orange conical nose. To
+// better create the impression they are sitting on the ground, we draw
+// a fake shadow, consisting of a dark circle under each.
+//
+// We make extensive use of nested transformations. Everything is drawn
+// relative to the origin. The snowman's eyes and nose are positioned
+// relative to a head sphere centered at the origin. Then the head is
+// translated into its final position. The body is drawn and translated
+// into its final position.
+//----------------------------------------------------------------------
+
+// =========================draw chessboard========================================
+
+void drawChessBoard()
+{
+    for(int i=0; i<8; i++)
+    {
+        for(int j=0; j<8; j++)
+        {
+            glPushMatrix();
+            glTranslatef(i*1,j*1,0);
+            if((i%2==0 && j%2==0)||(i%2==1 && j%2==1))
+            {
+                glColor3f(0.0,0.0,0.0);
+            }
+            else
+            {
+                glColor3f(0.6,0.6,0.6);
+            }
+            glBegin(GL_POLYGON);
+            glVertex3f(0.0,0.0,0.0);
+            glVertex3f(1.0,0.0,0.0);
+            glVertex3f(1.0,1.0,0.0);
+            glVertex3f(0.0,1.0,0.0);
+            glEnd();
+            glPopMatrix();
+        }
+    }
+}
+
+// =========================draw chessboard========================================
+
+
+
+// =========================draw chess pieces======================================
+// light pawn, glutSolidSphere
+void drawPawn(bool isLight)
+{
+    //draw big sphere
+
+    //draw edge
+
+    if(isLight) glColor3f(1.0, 1.0, 0.9569);
+    else glColor3f(0.5882, 0.2941, 0);
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0);
+    GLdouble eqn [4]={0.0,0.0,1.0,0.0};
+    glClipPlane(GL_CLIP_PLANE0,eqn);
+    glEnable(GL_CLIP_PLANE0);
+    glutSolidSphere(0.375, 20, 20);
+    glDisable(GL_CLIP_PLANE0);
+
+
+
+    // draw second big
+    //glColor3f(255, 255, 240); // set drawing color to white
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0.4); // position head
+    glutSolidSphere(0.25, 20, 20); // head sphere
+
+    // draw small
+   // glColor3f(150, 150, 150); // set drawing color to white
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0.35); // position head
+    glutSolidSphere(0.15, 20, 20); // head sphere
+
+
+    glPopMatrix();
+    glPopMatrix();
+    glPopMatrix();
+    glPopMatrix();
+
+}
+
+void drawRook(bool isLight)
+{
+
+
+    if(isLight) glColor3f(1.0, 1.0, 0.9569);
+    else glColor3f(0.5882, 0.2941, 0);
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0);
+    glutSolidCube(0.75);
+    glPopMatrix();
+
+}
+
+// draw knight glutSolidTeapot
+void drawKnight(bool isLight)
+{
+    if(isLight) glColor3f(1.0, 1.0, 0.9569);
+    else glColor3f(0.5882, 0.2941, 0);
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0);
+    GLdouble eqn [4]={0.0,0.0,1.0,0.0};
+    glClipPlane(GL_CLIP_PLANE0,eqn);
+    glEnable(GL_CLIP_PLANE0);
+    glutSolidTeapot(0.4);
+    glDisable(GL_CLIP_PLANE0);
+    glPopMatrix();
+}
+
+// draw bishop
+void drawBishop(bool isLight)
+{
+    if(isLight) glColor3f(1.0, 1.0, 0.9569);
+    else glColor3f(0.5882, 0.2941, 0);
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0);
+    glutSolidCone(0.375, 1, 20,20);
+    glPopMatrix();
+}
+// draw queen
+void drawQueen(bool isLight)
+{
+
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0.0);
+    if(isLight) glColor3f(1.0, 1.0, 0.9569);
+    else glColor3f(0.5882, 0.2941, 0);
+
+
+    glScalef(0.43,0.43,0.577);
+    glutSolidTetrahedron();
+
+    glPopMatrix();
+}
+
+// draw king
+void drawKing(bool isLight)
+{
+    if(isLight) glColor3f(1.0, 1.0, 0.9569);
+    else glColor3f(0.5882, 0.2941, 0);
+    glScalef(0.5,0.5,0.5);
+    glPushMatrix();
+    glTranslatef(0.0, 0.0, 0);
+    glutSolidOctahedron();
+
+    glPopMatrix();
+}
+// =========================draw chess pieces======================================
+
+
+
+
+
+
+//----------------------------------------------------------------------
+// Update with each idle event
+//
+// This incrementally moves the camera and requests that the scene be
+// redrawn.
+//----------------------------------------------------------------------
+void update(void)
+{
+    if (deltaMove) { // update camera position
+        x += deltaMove * lx * 0.1;
+        y += deltaMove * ly * 0.1;
+    }
+    glutPostRedisplay(); // redisplay everything
+}
+
+//----------------------------------------------------------------------
+// Draw the entire scene
+//
+// We first update the camera location based on its distance from the
+// origin and its direction.
+//----------------------------------------------------------------------
+void renderScene(void)
+{
+    int i, j;
+
+    // Clear color and depth buffers
+    glClearColor(0.0, 0.0, 0.0, 1.0); // sky color is light blue
+    glColor3f(0.0, 0.0, 0.0);
+    glPointSize(1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // Reset transformations
+    glLoadIdentity();
+
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_COLOR_MATERIAL);
+
+
+    //glEnable(GL_LIGHT1);
+    GLfloat light0_ambient[] = { 0.2, 0.2, 0.2, 1.0};
+    GLfloat light0_specular[] = { 0.8, 0.8, 0.8, 1.0};
+
+
+    GLfloat light1_diffuse[] = { 1.0, 1.0, 0.0, 1.0 };
+    GLfloat light1_position[] = { -5.0, -5.0, 8.0};
+    GLfloat mat_specular[] = { 1.0, 1.0, 1.0, 1.0 };
+    GLfloat mat_shininess[] = { 50.0 };
+    glShadeModel(GL_SMOOTH);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular);
+    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess);
+    //glLightfv(GL_LIGHT0, GL_POSITION, light1_position);
+
+
+    glLightfv(GL_LIGHT0, GL_AMBIENT, light0_ambient);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light0_specular);
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, light1_diffuse);
+    glLightfv(GL_LIGHT1, GL_POSITION, light1_position);
+    glEnable(GL_DEPTH_TEST);
+
+
+    // Set the camera centered at (x,y,1) and looking along directional
+    // vector (lx, ly, 0), with the z-axis pointing up
+    gluLookAt(
+            4, -5, 10+lz,
+            4, 4, 0.0,
+            0.0, 0.0, 1.0);
+    // draw board
+
+    drawChessBoard();
+    // ============== draw light part ===========================
+    // place 8 light pawn
+    for (i = 0; i < 8; i++)
+    {
+        glPushMatrix();
+        glTranslatef(i*1+0.5, 1.5, 0);
+        drawPawn(true);
+        glPopMatrix();
+
+    }
+
+    // place 2 light rook
+    for(i = 0; i < 2; i++)
+    {
+        glPushMatrix();
+        glTranslatef(i * 7 + 0.5,0.5,0.375);
+        drawRook(true);
+        glPopMatrix();
+    }
+
+
+    // place 2 knights_light
+    for(i = 0;i<2;i++)
+    {
+        glPushMatrix();
+        glTranslatef(i * 5 + 1.5,0.5,0.0);
+        drawKnight(true);
+        glPopMatrix();
+    }
+
+    // place 2 bishop
+    for(i = 0;i<2;i++)
+    {
+        glPushMatrix();
+        glTranslatef(i * 3 + 2.5,0.5,0.0);
+        drawBishop(true);
+        glPopMatrix();
+    }
+
+    // place light queen
+    glPushMatrix();
+    glTranslatef(3.5,0.5,0.5);
+    drawQueen(true);
+
+    glPopMatrix();
+
+    // place light king
+    glPushMatrix();
+    glTranslatef(4.5,0.5,0.5);
+    drawKing(true);
+
+    glPopMatrix();
+    // ============== draw light part ===========================
+
+
+    // ============== draw dark part ===========================
+    for (i = 0; i < 8; i++)
+    {
+        glPushMatrix();
+        glTranslatef(i*1+0.5, 6.5, 0);
+        drawPawn(false);
+        glPopMatrix();
+
+    }
+
+    // place 2 light rook
+    for(i = 0; i < 2; i++)
+    {
+        glPushMatrix();
+        glTranslatef(i * 7 + 0.5,7.5,0.375);
+        drawRook(false);
+        glPopMatrix();
+    }
+
+
+    // place 2 knights_light
+    for(i = 0;i<2;i++)
+    {
+        glPushMatrix();
+        glTranslatef(i * 5 + 1.5,7.5,0.0);
+        drawKnight(false);
+        glPopMatrix();
+    }
+
+    // place 2 bishop
+    for(i = 0;i<2;i++)
+    {
+        glPushMatrix();
+        glTranslatef(i * 3 + 2.5,7.5,0.0);
+        drawBishop(false);
+        glPopMatrix();
+    }
+
+    // place light queen
+    glPushMatrix();
+    glTranslatef(3.5,7.5,0.5);
+    drawQueen(false);
+
+    glPopMatrix();
+
+    // place light king
+    glPushMatrix();
+    glTranslatef(4.5,7.5,0.5);
+    drawKing(false);
+
+    glPopMatrix();
+    // ============== draw dark part ===========================
+
+    glutSwapBuffers(); // Make it all visible
+
+
+
+}
+
+//----------------------------------------------------------------------
+// User-input callbacks
+//
+// processNormalKeys: ESC, q, and Q cause program to exit
+// pressSpecialKey: Up arrow = forward motion, down arrow = backwards
+// releaseSpecialKey: Set incremental motion to zero
+//----------------------------------------------------------------------
+void processNormalKeys(unsigned char key, int xx, int yy)
+{
+    if (key == ESC || key == 'q' || key == 'Q')
+    {
+        exit(0);
+    }
+    else if(key == 'd' || key == 'D')
+    {
+        lz -= 0.25;
+    }
+    else if(key == 'u' || key == 'U')
+    {
+        lz += 0.25;
+    }
+    else if(key == '0')
+    {
+        if(light0) light0=false;
+        else light0=true;
+
+        if(light0) glEnable(GL_LIGHT0);
+        else glDisable(GL_LIGHT0);
+    }
+    else if(key == '1')
+    {
+
+        if(light1) light0=false;
+        else light1=true;
+
+        if(light1) glEnable(GL_LIGHT1);
+        else glDisable(GL_LIGHT1);
+    }
+}
+
+void pressSpecialKey(int key, int xx, int yy)
+{
+    switch (key)
+    {
+        case GLUT_KEY_UP: deltaMove = 1.0; break;
+        case GLUT_KEY_DOWN: deltaMove = -1.0; break;
+    }
+}
+
+void releaseSpecialKey(int key, int x, int y)
+{
+    switch (key)
+    {
+        case GLUT_KEY_UP: deltaMove = 0.0; break;
+        case GLUT_KEY_DOWN: deltaMove = 0.0; break;
+    }
+}
+
+//----------------------------------------------------------------------
+// Process mouse drag events
+//
+// This is called when dragging motion occurs. The variable
+// angle stores the camera angle at the instance when dragging
+// started, and deltaAngle is a additional angle based on the
+// mouse movement since dragging started.
+//----------------------------------------------------------------------
+void mouseMove(int x, int y)
+{
+    if (isDragging)
+    { // only when dragging
+        // update the change in angle
+        deltaAngle = (x - xDragStart) * 0.005;
+
+        // camera's direction is set to angle + deltaAngle
+        lx = -sin(angle + deltaAngle);
+        ly = cos(angle + deltaAngle);
+    }
+}
+
+void mouseButton(int button, int state, int x, int y)
+{
+    if (button == GLUT_LEFT_BUTTON)
+    {
+        if (state == GLUT_DOWN)
+        { // left mouse button pressed
+            isDragging = 1; // start dragging
+            xDragStart = x; // save x where button first pressed
+        }
+        else
+        { /* (state = GLUT_UP) */
+            angle += deltaAngle; // update camera turning angle
+            isDragging = 0; // no longer dragging
+        }
+    }
+}
+
+//----------------------------------------------------------------------
+// Main program  - standard GLUT initializations and callbacks
+//----------------------------------------------------------------------
+int main(int argc, char **argv)
+{
+    printf("\n\
+-----------------------------------------------------------------------\n\
+  OpenGL Sample Program:\n\
+  - Drag mouse left-right to rotate camera\n\
+  - Hold up-arrow/down-arrow to move camera forward/backward\n\
+  - q or ESC to quit\n\
+-----------------------------------------------------------------------\n");
+
+    // general initializations
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
+    glutInitWindowPosition(100, 100);
+    glutInitWindowSize(600, 600);
+    glutCreateWindow("Winter Wonderland");
+
+
+
+
+
+
+    // register callbacks
+    glutReshapeFunc(changeSize); // window reshape callback
+    glutDisplayFunc(renderScene); // (re)display callback
+    glutIdleFunc(update); // incremental update
+    glutIgnoreKeyRepeat(1); // ignore key repeat when holding key down
+    glutMouseFunc(mouseButton); // process mouse button push/release
+    glutMotionFunc(mouseMove); // process mouse dragging motion
+    glutKeyboardFunc(processNormalKeys); // process standard key clicks
+    glutSpecialFunc(pressSpecialKey); // process special key pressed
+    // Warning: Nonstandard function! Delete if desired.
+    glutSpecialUpFunc(releaseSpecialKey); // process special key release
+
+    // OpenGL init
+
+    glEnable(GL_DEPTH_TEST);
+
+
+
+
+    // enter GLUT event processing cycle
+    glutMainLoop();
+
+    return 0; // this is just to keep the compiler happy
+}
